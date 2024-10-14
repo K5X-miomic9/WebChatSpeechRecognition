2457c646b58d9d8a81a60cb1e6bc6a44d336fef9
K5X-miomic9 <183776582+K5X-miomic9@users.noreply.github.com>
2024-10-14 05:55:55 +0200

    .

--- src/WebChatSpeechRecognition.user.js
+++ src/WebChatSpeechRecognition.user.js
@@ -17,8 +17,8 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @require      https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.3.10/purify.min.js
-// @downloadURL  https://raw.githubusercontent.com/K5X-miomic9/WebChatSpeechRecognition/refs/heads/main/WebChatSpeechRecognition.user.js
-// @updateURL    https://raw.githubusercontent.com/K5X-miomic9/WebChatSpeechRecognition/refs/heads/main/WebChatSpeechRecognition.user.js
+// @downloadURL  https://github.com/K5X-miomic9/WebChatSpeechRecognition/blob/develop/src/WebChatSpeechRecognition.user.js
+// @updateURL    https://github.com/K5X-miomic9/WebChatSpeechRecognition/blob/develop/src/WebChatSpeechRecognition.user.js
 // ==/UserScript==
 
 (function () {
